abc36787504506b1c4978fd226c847663122271d
Rizzen Yazston <rizzen.yazston@gmail.com>
2025-06-21 16:35:15 +0200

    Initial commit at version 1.

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+// This file is part of `iced_experimental` project. For the terms of use, please see the file
+// called `LICENSE-BSD-3-Clause` at the top level of the `iced_experimental` project's Git repository.
+
+use iced_core as core;
+use iced_runtime as runtime;
+
